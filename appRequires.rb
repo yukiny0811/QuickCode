ba93487715fcc1b9711307af6d672c114e8e00ba
yuki <yukiny0811@gmail.com>
2021-03-06 18:50:07 +0900

    refactored all

---
+++ appRequires.rb
@@ -0,0 +1,23 @@
+require 'bundler/setup'
+Bundler.require
+require 'sinatra/reloader' if development?
+require 'sinatra/cookies'
+
+# load models
+require "./models/models.rb"
+require "./models/user.rb"
+require "./models/post.rb"
+require "./models/language.rb"
+require "./models/like.rb"
+require "./models/follow.rb"
+
+# load modules
+require "sass"
+require "pry"
+
+# configs
+enable :sessions
+set :sass, {:style => :compact }
+
+# load helpers
+require "./appHelpers"
